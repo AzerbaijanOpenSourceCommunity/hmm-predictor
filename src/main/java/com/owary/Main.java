9c7ffa3b92a5f2044f8e3e466aa350a23a26689e
Yusif Aliyev <yusif.projects@gmail.com>
2019-04-10 15:33:09 +0400

    Yusif joined the group

--- src/main/java/com/owary/Main.java
+++ src/main/java/com/owary/Main.java
@@ -1,13 +1,38 @@
 package com.owary;
 
-import com.owary.view.Texter;
+import com.owary.controller.Main_VC;
+import com.owary.model.Main_Model;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) throws IOException {
-        new Texter().run();
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("fxml/EditorView.fxml"));
+        loader.setControllerFactory(t -> new Main_VC(new Main_Model()));
+        Parent content = loader.load();
+
+        Scene scene = new Scene(content);
+
+        stage.setResizable(false);
+
+        stage.setScene(scene);
+        stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public static void main(String[] args) throws IOException {
+        //new Texter().run();
+        launch(args);
+    }
 }
---
+++ src/main/java/com/owary/model/Main_Model.java
@@ -0,0 +1,23 @@
+package com.owary.model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.scene.control.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main_Model {
+
+    private ObservableList list = FXCollections.observableArrayList();
+    private ArrayList<String> data = new ArrayList<String>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
+
+    public void loadData(ListView listView) {
+        list.removeAll();
+        list.addAll(data);
+
+        listView.getItems().addAll(list);
+    }
+
+}
